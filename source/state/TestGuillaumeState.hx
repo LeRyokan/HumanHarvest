2a2576d3702f1e30206be0618f1f58d611b04fcb
Guillaume Ambrois <Epono@users.noreply.github.com>
2017-11-29 11:22:38 +0100

    Séparation test state pour plus avoir tous les warnings de db

--- source/state/TestGuillaumeState.hx
+++ source/state/TestGuillaumeState.hx
@@ -2,96 +2,21 @@ package state;
 
 import flixel.FlxG;
 import flixel.FlxState;
-import flixel.math.FlxPoint;
-import state.TestGuillaumeState.User;
-import sys.db.Types;
 import flixel.util.FlxSave;
-import haxe.crypto.Base64;
 
 class TestGuillaumeState extends FlxState
 {
-	private var _cnx						: sys.db.Connection;
 	private var _gameSave					: FlxSave;
 
 	override public function create():Void
 	{
 		super.create();
-
-		// Open a connection
-		_cnx = sys.db.Sqlite.open("test.db");
-
-		// Set as the connection for our SPOD manager
-		sys.db.Manager.cnx = _cnx;
-		sys.db.Manager.initialize();
-
-		//cnx.close();
-	}
-
-	public function createTables():Void
-	{
-		if (!sys.db.TableCreate.exists(User.manager))
-		{
-			sys.db.TableCreate.create(User.manager);
-		}
-	}
-
-	public function insertNewUser():Void
-	{
-		var user1 = new User();
-		user1.name = "lol";
-		user1.value = 2;
-		user1.otherValue = 1.4;
-		user1.dateTime = Date.now();
-
-		user1.insert();
-
-		//trace(user1.name);
-
-		user1.name = "ah non";
-		user1.update();
-
-		//trace(user1.name);
-	}
-
-	public function searchAll():Void
-	{
-		for (user in User.manager.search($id > 0))
-		{
-			trace(user.toString());
-		}
-	}
-
-	public function clearAll():Void
-	{
-		User.manager.delete($id > 0);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 
-		//if (FlxG.keys.justPressed.C)
-		//{
-			//createTables();
-		//}
-		//if (FlxG.keys.justPressed.I)
-		//{
-			//insertNewUser();
-		//}
-		//if (FlxG.keys.justPressed.S)
-		//{
-			//searchAll();
-		//}
-		//if (FlxG.keys.justPressed.D)
-		//{
-			//clearAll();
-		//}
-		//if (FlxG.keys.justPressed.BACKSPACE)
-		//{
-			//// Close the connection
-			//_cnx.close();
-		//}
-		
 		if (FlxG.keys.justPressed.A)
 		{
 			loadSave();
@@ -126,20 +51,4 @@ class TestGuillaumeState extends FlxState
 		_gameSave.flush();
 	}
 	
-}
-
-class User extends sys.db.Object
-{
-	public static var manager = new sys.db.Manager<User>(User);
-
-	public var id : SId;
-	public var name : SString<32>;
-	public var value : SNull<SInt>;
-	public var otherValue : SNull<SFloat>;
-	public var dateTime : SDateTime;
-
-	override public function toString():String
-	{
-		return 'User [$id] : name = [$name], value = [$value], otherValue = [$otherValue], dateTime = [$dateTime]';
-	}
 }
